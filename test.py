f14e34e75b45ca6fcaf6805b9400ff882deeaf9a
Ricardo Martinez <rimtzg@gmail.com>
2019-01-16 17:48:23 -0600

    Update commit

---
+++ test.py
@@ -0,0 +1,43 @@
+import requests
+import threading
+import time
+from flask import Flask
+app = Flask(__name__)
+
+@app.before_first_request
+def activate_job():
+    def run_job():
+        while True:
+            print("Run recurring task")
+            time.sleep(3)
+
+    thread = threading.Thread(target=run_job)
+    thread.start()
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+def start_runner():
+    def start_loop():
+        not_started = True
+        while not_started:
+            print('In start loop')
+            try:
+                r = requests.get('http://127.0.0.1:5000/')
+                if r.status_code == 200:
+                    print('Server started, quiting start_loop')
+                    not_started = False
+                print(r.status_code)
+            except:
+                print('Server not yet started')
+            time.sleep(2)
+
+    print('Started runner')
+    thread = threading.Thread(target=start_loop)
+    thread.start()
+
+if __name__ == "__main__":
+    start_runner()
+    app.run()
